5ba34c53f725dbca3e05590ab658f36129662138
Philipp Piwo <github@piwo.space>
2020-02-13 21:11:24 +0100

    fixed contract, added tests

--- test/tippingPayfortxTest.js
+++ test/tippingPayfortxTest.js
@@ -6,7 +6,7 @@
  *  purpose with or without fee is hereby granted, provided that the above
  *  copyright notice and this permission notice appear in all copies.
  *
- *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ *  THE SOFTWARE IS PROVIDED 'AS IS' AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
  *  REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
  *  AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
  *  INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
@@ -14,7 +14,7 @@
  *  OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
  *  PERFORMANCE OF THIS SOFTWARE.
  */
-const {Universal, MemoryAccount, Node, Crypto} = require('@aeternity/aepp-sdk');
+const {Universal, MemoryAccount, Node} = require('@aeternity/aepp-sdk');
 
 const TIPPING_PAY_FOR_TX_CONTRACT = utils.readFileRelative('./contracts/tipping-payfortx.aes', 'utf-8');
 
@@ -25,7 +25,7 @@ const config = {
 };
 
 describe('Tipping Payfortx Contract', () => {
-    let contract;
+    let client, contract;
 
     before(async () => {
         client = await Universal({
@@ -45,6 +45,56 @@ describe('Tipping Payfortx Contract', () => {
         contract = await client.getContractInstance(TIPPING_PAY_FOR_TX_CONTRACT);
         const init = await contract.methods.init(wallets[0].publicKey);
         assert.equal(init.result.returnType, 'ok');
+    });
+
+    it('Tipping Payfortx Contract: Tip', async () => {
+        const tip = await contract.methods.tip('domain.test', 'Hello World', {amount : 100});
+        assert.equal(tip.result.returnType, 'ok');
+
+        const state = await contract.methods.get_state();
+
+        assert.equal(state.decodedResult.tip_urls[0][0], 'domain.test');
+        assert.deepEqual(state.decodedResult.tip_urls[0][1], [0]);
+
+        assert.equal(state.decodedResult.tips[0][0], 0);
+        assert.equal(state.decodedResult.tips[0][1].Tip[0].id, 0);
+        assert.equal(state.decodedResult.tips[0][1].Tip[0].sender, wallets[0].publicKey);
+        assert.equal(state.decodedResult.tips[0][1].Tip[0].repaid, false);
+        assert.equal(state.decodedResult.tips[0][1].Tip[0].url, 'domain.test');
+        assert.equal(state.decodedResult.tips[0][1].Tip[0].title, 'Hello World');
+        assert.equal(state.decodedResult.tips[0][1].Tip[0].amount, 100);
+        assert.deepEqual(state.decodedResult.tips[0][1].Tip[0].retip_ids, []);
+    });
+
+    it('Tipping Payfortx Contract: Retip', async () => {
+        const tip = await contract.methods.retip(0, {amount : 100});
+        assert.equal(tip.result.returnType, 'ok');
+
+        const state = await contract.methods.get_state();
+
+        assert.equal(state.decodedResult.tip_urls[0][0], 'domain.test');
+        assert.deepEqual(state.decodedResult.tip_urls[0][1], [1, 0]);
+
+        assert.equal(state.decodedResult.tips[1][1].ReTip[0].amount, 100);
+        assert.equal(state.decodedResult.tips[1][1].ReTip[0].sender, wallets[0].publicKey);
+        assert.equal(state.decodedResult.tips[1][1].ReTip[0].repaid, false);
+
+        assert.equal(state.decodedResult.tips[0][0], 0);
+        assert.equal(state.decodedResult.tips[0][1].Tip[0].amount, 100);
+        assert.deepEqual(state.decodedResult.tips[0][1].Tip[0].retip_ids, [1]);
+    });
+
+    it('Tipping Payfortx Contract: Claim', async () => {
+        const balanceBefore = await client.balance(wallets[1].publicKey);
+        const claim = await contract.methods.claim('domain.test', wallets[1].publicKey);
+        assert.equal(claim.result.returnType, 'ok');
+
+        const balanceAfter = await client.balance(wallets[1].publicKey);
+        assert.equal(parseInt(balanceBefore) + 200, parseInt(balanceAfter));
+
+        const state = await contract.methods.get_state();
+        assert.equal(state.decodedResult.tips[0][1].Tip[0].repaid, true);
+        assert.equal(state.decodedResult.tips[1][1].ReTip[0].repaid, true);
     });
 
 });
