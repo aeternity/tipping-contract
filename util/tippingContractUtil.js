ce30fbf9f0bf48aa0a9832c66f4eeaa5be08a2b5
Philipp Piwo <github@piwo.space>
2020-02-17 12:03:01 +0100

    added utils to extract data from contract state

---
+++ util/tippingContractUtil.js
@@ -0,0 +1,62 @@
+const BigNumber = require('bignumber.js');
+
+const tippingContractUtil = {};
+
+tippingContractUtil.getTipsRetips = (state) => {
+  const findUrl = (urlId) => state.urls.find(([_, id]) => urlId === id)[0];
+
+  const findClaimGen = (tipClaimGen, urlId) => {
+    const [_, data] = state.claims.find(([id, _]) => id === urlId);
+
+    return {
+      unclaimed: tipClaimGen > data[0],
+      claim_gen: data[0],
+      unclaimed_amount: data[1]
+    };
+  };
+
+  const findRetips = (tipId, urlId) => state.retips.filter(([_, data]) => data.tip_id === tipId).map(([id, data]) => {
+    data.id = id;
+    data.claim = findClaimGen(data.claim_gen, urlId);
+    return data;
+  });
+
+
+  const tips = state.tips.map(([id, data]) => {
+    data.id = id;
+    data.url = findUrl(data.url_id);
+    data.retips = findRetips(id, data.url_id);
+    data.claim = findClaimGen(data.claim_gen, data.url_id);
+
+    data.total_amount = new BigNumber(data.amount).plus(data.retips.reduce((acc, retip) => {
+      return acc.plus(retip.amount)
+    }, new BigNumber('0'))).toFixed();
+
+    data.total_unclaimed_amount = new BigNumber(data.claim.unclaimed ? data.amount : 0).plus(data.retips.reduce((acc, retip) => {
+      return acc.plus(retip.claim.unclaimed ? retip.amount : 0)
+    }, new BigNumber('0'))).toFixed();
+
+    return data;
+  });
+
+
+  const urls = state.urls.map(([url, id]) => {
+    const urlTips = tips.filter(tip => tip.url_id === id);
+    const claim = state.claims.find(([urlId, _]) => urlId === id)[1];
+
+    return {
+      url: url,
+      tip_ids: urlTips.map(tip => tip.id),
+      retip_ids: urlTips.flatMap(tip => tip.retips.map(retip => retip.id)),
+      unclaimed_amount: claim[1]
+    };
+  });
+
+
+  return {
+    urls: urls,
+    tips: tips
+  };
+};
+
+module.exports = tippingContractUtil;
