9b2c8a0012350b33eeb3d7d33c357eb684402fee
Philipp Piwo <github@piwo.space>
2020-10-28 10:56:05 +0100

    post without tipping contract first version

---
+++ test/tippingV3Test.js
@@ -0,0 +1,67 @@
+/*
+ * ISC License (ISC)
+ * Copyright (c) 2018 aeternity developers
+ *
+ *  Permission to use, copy, modify, and/or distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
+ *  THE SOFTWARE IS PROVIDED 'AS IS' AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ *  REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ *  AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ *  INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ *  LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ *  OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *  PERFORMANCE OF THIS SOFTWARE.
+ */
+const assert = require('chai').assert
+const {readFileRelative} = require('aeproject-utils/utils/fs-utils');
+const {defaultWallets: wallets} = require('aeproject-config/config/node-config.json');
+
+const {Universal, MemoryAccount, Node} = require('@aeternity/aepp-sdk');
+const TippingContractUtil = require('../util/tippingContractUtil');
+
+const TIPPING_CONTRACT = readFileRelative('./contracts/v3/Tipping_v3.aes', 'utf-8');
+const TIPPING_INTERFACE = readFileRelative('./contracts/v3/Tipping_v3_Interface.aes', 'utf-8');
+
+const config = {
+    url: 'http://localhost:3001/',
+    internalUrl: 'http://localhost:3001/',
+    compilerUrl: 'http://localhost:3080'
+};
+
+describe('Tipping V3 Contract', () => {
+    let client, contract;
+
+    before(async () => {
+        client = await Universal({
+            nodes: [{
+                name: 'devnetNode',
+                instance: await Node(config)
+            }],
+            accounts: [MemoryAccount({
+                keypair: wallets[0]
+            })],
+            networkId: 'ae_devnet',
+            compilerUrl: config.compilerUrl
+        });
+    });
+
+
+
+   it('Deploying Tipping Contract', async () => {
+        contract = await client.getContractInstance(TIPPING_CONTRACT);
+        const init = await contract.methods.init();
+        assert.equal(init.result.returnType, 'ok');
+    });
+
+   it('Tipping Contract Version', async () => {
+       assert.equal((await contract.methods.get_state()).decodedResult.version, "v3");
+   });
+
+    it('Tipping Contract Interface', async () => {
+        const interface = await client.getContractInstance(TIPPING_INTERFACE, {contractAddress: contract.deployInfo.address});
+        const state = await interface.methods.get_state();
+        assert.equal(state.result.returnType, 'ok');
+    });
+});
