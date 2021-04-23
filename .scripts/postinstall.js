8a5983e5d5cac86415f8d21ff0ac3ef6decf0a7c
Philipp Piwo <github@piwo.space>
2021-04-23 18:05:52 +0200

    add v1, v2 getter claim by url functions

--- .scripts/postinstall.js
+++ .scripts/postinstall.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 const tippingV1Interface = fs.readFileSync(__dirname + '/../contracts/v1/Tipping_v1_Interface.aes', 'utf-8');
 fs.writeFileSync(__dirname + '/../Tipping_v1_Interface.aes.js', `module.exports = \`\n${tippingV1Interface}\`;\n`, 'utf-8');
 
+const tippingV1Getter = fs.readFileSync(__dirname + '/../contracts/v1/Tipping_v1_Getter.aes', 'utf-8');
+fs.writeFileSync(__dirname + '/../Tipping_v1_Getter.aes.js', `module.exports = \`\n${tippingV1Getter}\`;\n`, 'utf-8');
+
 const tippingV2Interface = fs.readFileSync(__dirname + '/../contracts/v2/Tipping_v2_Interface.aes', 'utf-8');
 fs.writeFileSync(__dirname + '/../Tipping_v2_Interface.aes.js', `module.exports = \`\n${tippingV2Interface}\`;\n`, 'utf-8');
 
