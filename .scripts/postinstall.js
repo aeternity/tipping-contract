7ab2371dcb80d0f45b4a97642cd18a26de74e407
Philipp Piwo <github@piwo.space>
2021-06-30 12:03:18 +0200

    remove v2 getter

--- .scripts/postinstall.js
+++ .scripts/postinstall.js
@@ -9,9 +9,6 @@ fs.writeFileSync(__dirname + '/../Tipping_v1_Getter.aes.js', `module.exports = \
 const tippingV2Interface = fs.readFileSync(__dirname + '/../contracts/v2/Tipping_v2_Interface.aes', 'utf-8');
 fs.writeFileSync(__dirname + '/../Tipping_v2_Interface.aes.js', `module.exports = \`\n${tippingV2Interface}\`;\n`, 'utf-8');
 
-const tippingV2Getter = fs.readFileSync(__dirname + '/../contracts/v2/Tipping_v2_Getter.aes', 'utf-8');
-fs.writeFileSync(__dirname + '/../Tipping_v2_Getter.aes.js', `module.exports = \`\n${tippingV2Getter}\`;\n`, 'utf-8');
-
 const tippingV3Interface = fs.readFileSync(__dirname + '/../contracts/v3/Tipping_v3_Interface.aes', 'utf-8');
 fs.writeFileSync(__dirname + '/../Tipping_v3_Interface.aes.js', `module.exports = \`\n${tippingV3Interface}\`;\n`, 'utf-8');
 
